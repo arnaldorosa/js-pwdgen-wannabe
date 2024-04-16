42f21760e552797aba4cf83c9789e1b59aa5a100
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-04-16 15:55:17 +0200

    end exercise

---
+++ main.js
@@ -0,0 +1,10 @@
+let userName = prompt ("Inserisci il tuo nome");
+let userLastName = prompt ("Inserisci il tuo cognome");
+let userColor = prompt ("Inserisci il tuo colore preferito");
+
+const userPassword = userName + userLastName + userColor + "24";
+
+document.getElementById('result-name').innerHTML = "Il mio nome è" + " " + userName;
+document.getElementById('result-lastname').innerHTML = "Il mio cognome è" + " " + userLastName;
+document.getElementById('result-color').innerHTML = "Il mio colore preferito è" + " " + userColor;
+document.getElementById('reult-password').innerHTML = "La mia password è" + " " + userPassword;
